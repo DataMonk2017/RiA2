de6c327ed70d75240afcb0b0ccf687448d499d47
Jeremy Zheng <DataMonk2017@users.noreply.github.com>
2017-06-18 16:44:15 -0400

    Update Ch01 Introduction to R.R

--- Ch01 Introduction to R.R
+++ Ch01 Introduction to R.R
@@ -3,6 +3,44 @@
 # Introduction to R                  #
 #------------------------------------#
 
+#General help.
+help.start()
+
+#Help on function read.csv (the quotation marks are optional).
+?read.csv
+help('read.csv')
+
+
+data()#List all available example datasets contained in currently loaded packages.
+vignette()#List all available vignettes for currently installed packages.
+vignette('FAQ')#Display specific vignettes for topic 'FAQ'.
+
+
+getwd()#List hte current working directory.
+setwd('my directory')#Change the current working directory to my directory.
+ls()#List the objects in the current workspace
+rm(objectlist) #Remove(delete) one or more objects
+help(options)#learn about available options
+options()#View or set current options.
+
+
+#history(#)#Display your last # commands (default=25)
+savehistory('myfile')#Save the commands history to myfile (default = .Rhistory)
+loadhistory('myfile')#Reload a command's history(default = .Rhistory)
+save.image('myfile')#Save the workspace to my file (default =.RData)
+save(objectlist,file = 'myfile')#save specific objects to a file
+load("my file")#load a workspace into the currrent seesion
+q()#quit R
+
+#fuctions for saving graphic output
+pdf("filename.pdf") #PDF file
+win.metafile("filename.wmf") #Windows metafile
+png("filename.png") #PBG file
+jpeg("filename.jpg") #JPEG file
+bmp("filename.bmp") #BMP file
+postscript("filename.ps") #PostScript file
+
+
 # A sample R session
 # q() is commented out so you don't accidently exit
 
@@ -40,3 +78,11 @@ help(Arthritis)
 Arthritis
 example(Arthritis)
 # q()
+
+source("script1.R")
+sink("myoutput",append=True, split=True)
+pdf('mygraphs.pdf')
+scource('script2.R')
+sink()
+dev.off()
+source('script3.R')
